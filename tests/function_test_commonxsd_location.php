467e85f3c337ed955d88f98bdb578136313935f7
Carlos C Soto <eclipxe13@gmail.com>
2015-08-11 14:05:16 -0500

    function test_commonxsd_location trigger a E_USER_ERROR if the commonxsd folder is not found

--- tests/function_test_commonxsd_location.php
+++ tests/function_test_commonxsd_location.php
@@ -2,9 +2,10 @@
 
 function test_commonxsd_location($filename)
 {
-    $dirname = realpath(__DIR__ . '/../commonxsd');
-    if (! is_dir($dirname)) {
-        trigger_error('Missing folder commonxsd', E_USER_ERROR);
+    $location = __DIR__ . '/../commonxsd';
+    $dirname = realpath($location);
+    if (!$dirname or !is_dir($dirname)) {
+        trigger_error("Missing folder commonxsd: $location", E_USER_ERROR);
     }
     return $dirname . '/' . $filename;
 }
