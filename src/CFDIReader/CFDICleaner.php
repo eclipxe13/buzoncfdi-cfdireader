81d860098cd72e31fd6102bfb600299815e5400f
Carlos C Soto <eclipxe13@gmail.com>
2016-05-06 17:19:10 -0500

    Include CFDICleaner, an utility to avoid must common issues on CFDI

---
+++ src/CFDIReader/CFDICleaner.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace CFDIReader;
+
+use DOMDocument;
+use DOMNode;
+use DOMNodeList;
+use DOMXPath;
+
+class CFDICleanerException extends \RuntimeException {}
+
+
+/**
+ * Class to clean CFDI and avoid bad practices
+ * Strictly speaking, CFDI must acomplish all XML rules, including that any other
+ * xml element must be isolated in its own schema and follow their own rules
+ * The common practice (allowed by SAT) is that the CFDI is created, signed and after
+ * some nodes are attatched, some of them does not accomply the XML Schemas
+ * This is why it's better to clear Comprobante/Addenda and remove unused namespaces
+ * @package CFDIReader
+ */
+class CFDICleaner
+{
+
+    /** @var DOMDocument */
+    protected $dom;
+
+    /**
+     * CFDICleaner constructor.
+     * @param string $content
+     */
+    public function __construct($content)
+    {
+        if (!empty($content)) $this->loadContent($content);
+    }
+
+    /**
+     * Method to clean content and return the result
+     * If an error occurs, an exception is thrown
+     * @param string $content
+     * @return string
+     */
+    public static function staticClean($content)
+    {
+        $cleaner = new self($content);
+        $cleaner->clean();
+        return $cleaner->retrieveXml();
+    }
+
+    /**
+     * Check if the CFDI version is complatible to this class
+     * @param string $version
+     * @return bool
+     */
+    public static function isVersionAllowed($version)
+    {
+        return in_array($version, ["3.2"]);
+    }
+
+    /**
+     * Check if a given namespace is allowed (must not be removed from CFDI)
+     * @param string $namespace
+     * @return bool
+     */
+    public static function isNameSpaceAllowed($namespace)
+    {
+        $fixedNS = [
+            "http://www.w3.org/2001/XMLSchema-instance",
+            "http://www.w3.org/XML/1998/namespace",
+        ];
+        foreach($fixedNS as $ns) {
+            if (0 === strcasecmp($ns, $namespace)) {
+                return true;
+            }
+        }
+        $willcardNS = [
+            "http://www.sat.gob.mx/",
+        ];
+        foreach($willcardNS as $ns) {
+            if (0 === strpos($namespace, $ns)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    /**
+     * Apply all removals (Addenda, Non SAT Nodes and Non SAT namespaces)
+     */
+    public function clean()
+    {
+        $this->removeAddenda();
+        $this->removeNonSatNSNodes();
+        $this->removeNonSatNSschemaLocations();
+    }
+
+    /**
+     * Load the string content as a CFDI
+     * This is exposed to reuse the current object instead of create a new instance
+     * @param string $content
+     */
+    public function loadContent($content)
+    {
+        $dom = new DOMDocument();
+        $dom->loadXML($content, LIBXML_ERR_WARNING);
+        if (true == $loaderror = libxml_get_last_error()) {
+            throw new CFDICleanerException('XML Error: ' . $loaderror);
+        }
+        $prefix = $dom->lookupPrefix("http://www.sat.gob.mx/cfd/3");
+        if (!$prefix) {
+            throw new CFDICleanerException('The XML document is not a CFDI');
+        }
+        $version = $this->xpathQuery('/' . $prefix . ':Comprobante/@version', $dom->documentElement);
+        if ($version->length != 1) {
+            throw new CFDICleanerException('The XML document does not contains a version');
+        }
+        if (!$this->isVersionAllowed($version->item(0)->nodeValue)) {
+            throw new CFDICleanerException('The XML document version "' . $version->item(0)->nodeValue . '" is not compatible');
+        }
+        $this->dom = $dom;
+    }
+
+    /**
+     * Get the XML content of the CFDI
+     * @return string
+     */
+    public function retrieveXml()
+    {
+        return $this->dom->saveXML();
+    }
+
+    /**
+     * Helper function to Perform a XPath Query using
+     * @param string $query
+     * @param DOMNode|null $element
+     * @return DOMNodeList
+     */
+    private function xpathQuery($query, DOMNode $element = null)
+    {
+        $element = $element ?: $this->dom->documentElement;
+        return (new DOMXPath($element->ownerDocument))->query($query, $element);
+    }
+
+    /**
+     * Procedure to remove the Comprobante/Addenda node
+     */
+    public function removeAddenda()
+    {
+        $prefix = $this->dom->lookupPrefix("http://www.sat.gob.mx/cfd/3");
+        $query = '/' . $prefix . ':Comprobante/' . $prefix . ':Addenda';
+        $addendas = $this->xpathQuery($query);
+        if ($addendas->length == 0) return;
+        for($i = 0 ; $i < $addendas->length ; $i++) {
+            $addenda = $addendas->item($i);
+            $addenda->parentNode->removeChild($addenda);
+        }
+    }
+    
+    /**
+     * Procedure to remove all nodes that are not allowed and clean those namespaces from the schemaLocations
+     * It is a shortcut to remove Nodes and schemaLocations 
+     */
+    public function removeNonSatNS()
+    {
+        $this->removeNonSatNSNodes();
+        $this->removeNonSatNSschemaLocations();
+    }
+
+    /**
+     * Procedure to drop schemaLocations that are not allowed
+     * If the schemaLocation is empty then remove the attribute
+     */
+    public function removeNonSatNSschemaLocations()
+    {
+        $xsi = $this->dom->lookupPrefix("http://www.w3.org/2001/XMLSchema-instance");
+        if (!$xsi) return;
+        $schemaLocations = $this->xpathQuery("//@$xsi:schemaLocation");
+        if (false === $schemaLocations or $schemaLocations->length === 0) return;
+        for($s = 0 ; $s < $schemaLocations->length ; $s++) {
+            $this->removeNonSatNSschemaLocation($schemaLocations->item($s));
+        }
+    }
+
+    /**
+     * @param DOMNode $schemaLocation This is the attribute
+     */
+    private function removeNonSatNSschemaLocation(DOMNode $schemaLocation)
+    {
+        $source = $schemaLocation->nodeValue;
+        $parts = array_values(array_filter(explode(' ', $source)));
+        if (0 !== count($parts) % 2) {
+            throw new CFDICleanerException("The schemaLocation value '" . $source . "' must have even number of URIs");
+        }
+        $modified = "";
+        for ($k = 0; $k < count($parts); $k = $k + 2) {
+            if (!$this->isNameSpaceAllowed($parts[$k])) continue;
+            $modified .= $parts[$k] . " " . $parts[$k + 1] . " ";
+        }
+        $modified = rtrim($modified, " ");
+        if ($source == $modified) return;
+        if (" " !== $modified) {
+            $schemaLocation->nodeValue = $modified;
+        } else {
+            $schemaLocation->parentNode->attributes->removeNamedItemNS(
+                $schemaLocation->namespaceURI, $schemaLocation->nodeName
+            );
+        }
+    }
+
+    /**
+     * Procedure to remove all nodes that are not allowed
+     */
+    public function removeNonSatNSNodes()
+    {
+        $nss = [];
+        foreach($this->xpathQuery('//namespace::*') as $node) {
+            $namespace = $node->nodeValue;
+            if ($this->isNameSpaceAllowed($namespace)) continue;
+            $nss[] = $namespace;
+        }
+        if (!count($nss)) return;
+        foreach($nss as $namespace) {
+            $this->removeNonSatNSNode($namespace);
+        }
+    }
+
+    /**
+     * Procedure to remove all nodes from an specific namespace
+     * @param string $namespace
+     */
+    private function removeNonSatNSNode($namespace)
+    {
+        foreach($this->dom->getElementsByTagNameNS($namespace, "*") as $children) {
+            $children->parentNode->removeChild($children);
+        }
+    }
+
+}
---
+++ tests/CFDIReaderTests/CFDICleanerTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace CFDIReaderTests;
+
+use CFDIReader\CFDICleaner;
+use CFDIReader\CFDICleanerException;
+
+class CFDICleanerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructorWithEmptyText()
+    {
+        $cleaner = new CFDICleaner("");
+        $this->setExpectedException(CFDICleanerException::class);
+        // use the @ to not throw the warning
+        @$cleaner->loadContent("");
+    }
+
+    public function xxtestConstructorWithNonCFDI()
+    {
+        $cleaner = new CFDICleaner("");
+        $this->setExpectedException(CFDICleanerException::class);
+        // use the @ to not throw the warning
+        @$cleaner->loadContent('<cfdi></cfdi>');
+    }
+
+    public function testConstructorWithBadVersion()
+    {
+        $this->setExpectedException(CFDICleanerException::class);
+        new CFDICleaner('<?xml version="1.0" encoding="UTF-8"?>
+            <'.'cfdi:Comprobante xmlns:cfdi="http://www.sat.gob.mx/cfd/3" version="3.15" />
+        ');
+    }
+
+    public function testConstructorWithMinimalCompatibility()
+    {
+        $cleaner = new CFDICleaner('<?xml version="1.0" encoding="UTF-8"?>
+            <'.'cfdi:Comprobante xmlns:cfdi="http://www.sat.gob.mx/cfd/3" version="3.2" />
+        ');
+        $this->assertInstanceOf(CFDICleaner::class, $cleaner, 'Cleaner created with minimum compatibility');
+    }
+
+    public function testCleanOnDetail()
+    {
+        $basefile = test_file_location('cfdi-cleaner-dirty.xml');
+        $step1 = test_file_location('cfdi-cleaner-no-addenda.xml');
+        $step2 = test_file_location('cfdi-cleaner-no-nonsat-nodes.xml');
+        $step3 = test_file_location('cfdi-cleaner-no-nonsat-schemalocations.xml');
+        foreach([$basefile, $step1, $step2, $step3] as $filename) {
+            $this->assertFileExists($basefile, "The file $filename for testing does not exists");
+        }
+        $cleaner = new CFDICleaner(file_get_contents($basefile));
+        $this->assertXmlStringEqualsXmlFile($basefile, $cleaner->retrieveXml(), 'Compare that the document was loaded without modifications');
+        $cleaner->removeAddenda();
+        $this->assertXmlStringEqualsXmlFile($step1, $cleaner->retrieveXml(), 'Compare that addenda was removed');
+        $cleaner->removeNonSatNSNodes();
+        $this->assertXmlStringEqualsXmlFile($step2, $cleaner->retrieveXml(), 'Compare that non SAT nodes were removed');
+        $cleaner->removeNonSatNSschemaLocations();
+        $this->assertXmlStringEqualsXmlFile($step3, $cleaner->retrieveXml(), 'Compare that non SAT schemaLocations were removed');
+        $this->assertXmlStringEqualsXmlFile($step3, CFDICleaner::staticClean(file_get_contents($basefile)), 'Check static method for cleaning is giving the same results as detailed execution');
+    }
+
+}
+
