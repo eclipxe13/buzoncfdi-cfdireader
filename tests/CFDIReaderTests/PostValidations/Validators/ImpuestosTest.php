f8404794b99b3581be9de71a80c0de6eb9e9418d
David Arzate <driftking301@gmail.com>
2017-08-23 09:48:49 -0500

    Test validator impuestos with errors

--- tests/CFDIReaderTests/PostValidations/Validators/ImpuestosTest.php
+++ tests/CFDIReaderTests/PostValidations/Validators/ImpuestosTest.php
@@ -15,4 +15,56 @@ class ImpuestosTest extends ValidatorsTestCase
 
         $this->assertCount(0, $this->issues->all());
     }
+
+    public function testValidateRetenidos()
+    {
+        $this->setupWithFile('cfdi-impuestos-retenidos.xml');
+
+        $validator = new Impuestos();
+        $validator->validate($this->cfdi, $this->issues);
+
+        $expectedMessage = 'El total de impuestos retenidos difiere de la suma de los nodos de las retenciones';
+
+        $this->assertContains($expectedMessage, $this->issues->messages(IssuesTypes::WARNING)->all());
+        $this->assertCount(1, $this->issues->all());
+    }
+
+    public function testValidateTrasladados()
+    {
+        $this->setupWithFile('cfdi-impuestos-trasladados.xml');
+
+        $validator = new Impuestos();
+        $validator->validate($this->cfdi, $this->issues);
+
+        $expectedMessage = 'El total de impuestos trasladados difiere de la suma de los nodos de los traslados';
+
+        $this->assertContains($expectedMessage, $this->issues->messages(IssuesTypes::WARNING)->all());
+        $this->assertCount(1, $this->issues->all());
+    }
+
+    public function testValidateLocalesRetenidos()
+    {
+        $this->setupWithFile('cfdi-impuestos-locales-retenidos.xml');
+
+        $validator = new Impuestos();
+        $validator->validate($this->cfdi, $this->issues);
+
+        $expectedMessage = 'El total de impuestos locales retenidos difiere de la suma de los nodos de las retenciones';
+
+        $this->assertContains($expectedMessage, $this->issues->messages(IssuesTypes::WARNING)->all());
+        $this->assertCount(1, $this->issues->all());
+    }
+
+    public function testValidateLocalesTrasladados()
+    {
+        $this->setupWithFile('cfdi-impuestos-locales-trasladados.xml');
+
+        $validator = new Impuestos();
+        $validator->validate($this->cfdi, $this->issues);
+
+        $expectedMessage = 'El total de impuestos locales trasladados difiere de la suma de los nodos de los traslados';
+
+        $this->assertContains($expectedMessage, $this->issues->messages(IssuesTypes::WARNING)->all());
+        $this->assertCount(1, $this->issues->all());
+    }
 }
