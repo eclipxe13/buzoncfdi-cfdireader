f96e927b4d4893ae4c90d44ab8ad9a10a70b8524
Carlos C Soto <eclipxe13@gmail.com>
2017-09-27 13:12:39 -0500

    Add docblock for validator and a brief explanation of how to use it

--- src/CFDIReader/PostValidations/ValidatorInterface.php
+++ src/CFDIReader/PostValidations/ValidatorInterface.php
@@ -5,5 +5,15 @@ use CFDIReader\CFDIReader;
 
 interface ValidatorInterface
 {
+    /**
+     * Method to perform the validation on the CFDIReader.
+     * If the validation process found some issue then it must append it to the issues object as:
+     * - Error: When the CFDI should be revoked.
+     * - Warning: When the CFDI contains somethind you notice but is no reason to revoke the document.
+     *
+     * @param CFDIReader $cfdi
+     * @param Issues $issues
+     * @return void
+     */
     public function validate(CFDIReader $cfdi, Issues $issues);
 }
